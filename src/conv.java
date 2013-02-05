e8f37f2f5d4af1e2033618ef22d1a0141e48f8ef
root <root@joevin-HP-G62-Notebook-PC.(none)>
2013-02-05 01:40:26 +0000

    first

---
+++ src/conv.java
@@ -0,0 +1,14 @@
+
+public class conv {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Convertiseur a=new Convertiseur();
+		System.out.println(a.facto(24));
+		// TODO Auto-generated method stub
+
+	}
+
+}
